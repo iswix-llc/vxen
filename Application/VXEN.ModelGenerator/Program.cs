e117195f2df93c89b1d6d0a62abc4f8181cbc807
Christopher Painter <chrpai@iswix.com>
2018-11-13 13:26:02 -0600

    Create utility to download xsd and generate class files instead of msbuild

---
+++ Application/VXEN.ModelGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VXEN.ModelGenerator.Steps;
+
+namespace VXEN.ModelGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set Current Directory to EXE location
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileInfo fileInfo = new FileInfo(assembly.Location);
+            Environment.CurrentDirectory = fileInfo.Directory.FullName;
+
+            // Download Schemas from vendor
+            Download.GenerateClasses();
+
+
+        }
+    }
+}
---
+++ Application/VXEN.ModelGenerator/Steps/Download.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VXEN.ModelGenerator.Steps
+{
+    class Download
+    {
+
+        public static void GenerateClasses()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFile("https://transaction.elementexpress.com/express.xsd", @"Transaction\express.xsd");
+                wc.DownloadFile("https://services.elementexpress.com/expressservices.xsd", @"Services\expressservices.xsd");
+                wc.DownloadFile("https://reporting.elementexpress.com/expressreporting.xsd", @"Reporting\expressreporting.xsd");
+            }
+
+            string xsdPath = @"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\xsd.exe";
+            Process.Start(xsdPath, @"Transaction\express.xsd /classes /namespace:VXEN.Models.Transaction");
+            Process.Start(xsdPath, @"Reporting\expressreporting.xsd /classes /namespace:VXEN.Models.Reporting");
+            Process.Start(xsdPath, @"Services\expresservices.xsd /classes /namespace:VXEN.Models.Services");
+        }
+    }
+}

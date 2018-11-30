5db75e0b73e29a5a816efc6dda3a6a981585584d
Christopher Painter <chrpai@iswix.com>
2018-11-29 18:33:59 -0600

    Update ResponseHelper.cs

--- Application/VXEN.Services/ResponseHelper.cs
+++ Application/VXEN.Services/ResponseHelper.cs
@@ -26,11 +26,21 @@ namespace VXEN.Services
             return data;
         }
 
+        public static List<string> GetElementValuesFromResponse(this XDocument responseDocument, string elementName)
+        {
+
+            var elements = from e in responseDocument.Descendants()
+                           where e.Name.LocalName == elementName
+                           select e.Value;
+            return elements.ToList<string>();
+        }
+
         public static Dictionary<string,string> GetResponseAsDictionary(this XDocument responseDocument)
         {
             var elements = from e in responseDocument.Descendants()
                             select e;
             return elements.ToDictionary(o => o.Name.LocalName, o => o.Value);
         }
+
     }
 }
